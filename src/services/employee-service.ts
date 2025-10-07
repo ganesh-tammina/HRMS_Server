432f3347b698dbad345ef66e592ca3dab834edac
Prasanthdarla30 <prasanthdarla66@gmail.com>
2025-10-07 17:16:30 +0530

    commit interfaces

--- src/services/employee-service.ts
+++ src/services/employee-service.ts
@@ -3,6 +3,75 @@ import { Request, Response } from "express";
 import { EmployeesInterface } from "../interface/employee-interface";
 
 export default class Employeeservices implements EmployeesInterface {
+  deleteEmployees(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  viewEmployees(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  addAddress(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  editAddress(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  deleteAddress(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  viewAddress(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  addEmployement_details(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  editEmployement_details(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  deleteEmployement_details(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  viewEmployement_details(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  addExitdetails(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  viewExitdetails(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  deleteExitdetails(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  editExitdetails(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  addFamilyInfo(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  viewFamilyInfo(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  deleteFamilyInfo(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  editFamilyInfo(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  addStatutoryInfo(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  viewStatutoryInfo(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  deleteStatutoryInfo(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  editStatutoryInfo(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  editEmployees(req: Request, res: Response): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
   addEmployees(req: Request, res: Response): Promise<any> {
       throw new Error("Method not implemented.");
   }
