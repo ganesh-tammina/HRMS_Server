c2982b680e87117038285c879075fede2b9e921e
bipulsardar421 <bipulsardar421@gmail.com>
2025-10-09 10:14:17 +0530

    nn

--- src/controller/employee-controller.ts
+++ src/controller/employee-controller.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
-import EmployeeService from "../services/employee-service";
 import Employeeservices from "../services/employee-service";
 
-const employeeService = new EmployeeService();
+const employeeService = new Employeeservices();
 
 export default class EmployeeController {
   static async insertEmployee(req: Request, res: Response) {
-    const result = await employeeService.addEmployees(req,res);
+    const result = await employeeService.addEmployees(req, res);
+    return res.status(result.statusCode).json(result);
+  }
+  static async insertEmployeeAddress(req: Request, res: Response) {
+    const addType = "Current";
+    const result = await employeeService.addAddress(req, res, addType);
     return res.status(result.statusCode).json(result);
   }
 }
--- src/interface/employee-interface.ts
+++ src/interface/employee-interface.ts
@@ -1,41 +1,43 @@
 import { Request, Response } from "express";
 
 export interface promised {
-    success: boolean,
-    statusCode: number,
-    message: string
-    data?: {} | string,
-    error?: {} | string
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data?: {} | string;
+  error?: {} | string;
 }
 export interface EmployeesInterface {
-    addEmployees(req: Request, res: Response): Promise<promised>;
-    editEmployees(req: Request, res: Response): Promise<any>;
-    deleteEmployees(req: Request, res: Response): Promise<any>;
-    viewEmployees(req: Request, res: Response): Promise<any>;
-    addAddress(req: Request, res: Response): Promise<any>;
-    editAddress(req: Request, res: Response): Promise<any>;
-    deleteAddress(req: Request, res: Response): Promise<any>;
-    viewAddress(req: Request, res: Response): Promise<any>;
-    addEmployement_details(req: Request, res: Response): Promise<any>;
-    editEmployement_details(req: Request, res: Response): Promise<any>;
-    deleteEmployement_details(req: Request, res: Response): Promise<any>;
-    viewEmployement_details(req: Request, res: Response): Promise<any>;
-    addExitdetails(req: Request, res: Response): Promise<any>;
-    viewExitdetails(req: Request, res: Response): Promise<any>;
-    deleteExitdetails(req: Request, res: Response): Promise<any>;
-    editExitdetails(req: Request, res: Response): Promise<any>;
-    addFamilyInfo(req: Request, res: Response): Promise<any>;
-    viewFamilyInfo(req: Request, res: Response): Promise<any>;
-    deleteFamilyInfo(req: Request, res: Response): Promise<any>;
-    editFamilyInfo(req: Request, res: Response): Promise<any>;
-    addStatutoryInfo(req: Request, res: Response): Promise<any>;
-    viewStatutoryInfo(req: Request, res: Response): Promise<any>;
-    deleteStatutoryInfo(req: Request, res: Response): Promise<any>;
-    editStatutoryInfo(req: Request, res: Response): Promise<any>;
-
+  addEmployees(req: Request, res: Response): Promise<promised>;
+  editEmployees(req: Request, res: Response): Promise<promised>;
+  deleteEmployees(req: Request, res: Response): Promise<promised>;
+  viewEmployees(req: Request, res: Response): Promise<promised>;
+  addAddress(
+    req: Request,
+    res: Response,
+    addType: "Current" | "Permanent"
+  ): Promise<promised>;
+  editAddress(req: Request, res: Response): Promise<promised>;
+  deleteAddress(req: Request, res: Response): Promise<promised>;
+  viewAddress(req: Request, res: Response): Promise<promised>;
+  addEmployement_details(req: Request, res: Response): Promise<promised>;
+  editEmployement_details(req: Request, res: Response): Promise<promised>;
+  deleteEmployement_details(req: Request, res: Response): Promise<promised>;
+  viewEmployement_details(req: Request, res: Response): Promise<promised>;
+  addExitdetails(req: Request, res: Response): Promise<promised>;
+  viewExitdetails(req: Request, res: Response): Promise<promised>;
+  deleteExitdetails(req: Request, res: Response): Promise<promised>;
+  editExitdetails(req: Request, res: Response): Promise<promised>;
+  addFamilyInfo(req: Request, res: Response): Promise<promised>;
+  viewFamilyInfo(req: Request, res: Response): Promise<promised>;
+  deleteFamilyInfo(req: Request, res: Response): Promise<promised>;
+  editFamilyInfo(req: Request, res: Response): Promise<promised>;
+  addStatutoryInfo(req: Request, res: Response): Promise<promised>;
+  viewStatutoryInfo(req: Request, res: Response): Promise<promised>;
+  deleteStatutoryInfo(req: Request, res: Response): Promise<promised>;
+  editStatutoryInfo(req: Request, res: Response): Promise<promised>;
 }
 export interface LoginInterface {
-    login(req: Request, res: Response): Promise<any>;
-    emailCheck(req: Request, res: Response): Promise<any>;
-
+  login(req: Request, res: Response): Promise<any>;
+  emailCheck(req: Request, res: Response): Promise<any>;
 }
