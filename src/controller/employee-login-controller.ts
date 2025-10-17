15d17db0df70cae521b55d3de14ca33042ea6429
ganesh-tammina <82147644+ganesh-tammina@users.noreply.github.com>
2025-10-17 11:49:29 +0530

    made changes in server

--- src/controller/employee-login-controller.ts
+++ src/controller/employee-login-controller.ts
@@ -1,16 +1,37 @@
 import LoginService from "../services/employee-login-service";
 import { Request, Response } from "express";
 export default class EmployeeLoginController {
-    public static async EmailCheck(req: Request, res: Response) {
-        try {
-            const loginService = new LoginService();
-            await loginService.emailCheck(req, res);
-        } catch (error: any) {
-            console.error("Error in email-check route:", error);
-            return res.status(500).json({
-                success: false,
-                message: "Internal server error in email-check route.",
-            });
-        }
+  public static async EmailCheck(req: Request, res: Response) {
+    try {
+      await LoginService.emailCheck(req, res);
+    } catch (error: any) {
+      console.error("Error in email-check route:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error in email-check route.",
+      });
     }
+  }
+  public static async PasswordGeneratorHey(req: Request, res: Response) {
+    try {
+      await LoginService.passwordGen(req, res);
+    } catch (error: any) {
+      console.error("Error in login route:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error in login route.",
+      });
+    }
+  }
+  public static async Login(req: Request, res: Response) {
+    try {
+      await LoginService.login(req, res);
+    } catch (error: any) {
+      console.error("Error in login route:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error in login route.",
+      });
+    }
+  }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,23 +6,27 @@ import multer from "multer";
 import { ExcelProcessorService } from "../services/excel-processer-service";
 import EmployeeLoginController from "../controller/employee-login-controller";
 import AdminController from "../controller/admin-controller";
-import { checkWhoAmI } from "../middlewares/cookie-parser-middleware";
+import { checkIfIamEmployeeAtAll, checkIfIamValidEmployee, checkWhoAmI } from "../middlewares/cookie-parser-middleware";
 import EmployeeController from "../controller/employee-controller";
+import LoginService from "../services/employee-login-service";
 
 const upload = multer({ dest: "uploads/" });
 
 const router = Router();
 router.post("/v1/send-email", MailController.mailsender);
-router.post("/v1/login", EmployeeLoginController.EmailCheck);
+router.post("/v1/check-email",checkWhoAmI, EmployeeLoginController.EmailCheck);
+router.post("/v1/gen-password", checkIfIamValidEmployee, EmployeeLoginController.PasswordGeneratorHey)
+router.post("/v1/login", checkIfIamEmployeeAtAll, EmployeeLoginController.Login)
 router.post("/v1/addEmployee", EmployeeController.insertEmployee);
-router.post("/v1/addEmployementDetails",EmployeeController.insertEmploymentDetails);
-router.post("/v1/add-Statutory-Info",EmployeeController.insertEmployeeStatutoryInfo);
-router.post("/v1/add-Employee-Family-Info",EmployeeController.insertEmployeeFamilyInfo);
-router.post("/v1/parse-excel",upload.single("file"),AdminController.uploadExcel);
+router.post("/v1/addEmployementDetails", EmployeeController.insertEmploymentDetails);
+router.post("/v1/add-Statutory-Info", EmployeeController.insertEmployeeStatutoryInfo);
+router.post("/v1/add-Employee-Family-Info", EmployeeController.insertEmployeeFamilyInfo);
+router.post("/v1/parse-excel", upload.single("file"),AdminController.uploadExcel);
 router.post("/v1/addExitDetails", EmployeeController.insertExitDetails);
-router.post("/v1/current-address",EmployeeController.insertEmployeeCurrentAddress);
-router.post("/v1/permanent-address",EmployeeController.insertEmployeePermanentAddress);
-router.post("/v1/bulk-data-entry",EmployeeController.insertBulkEmployees);
+router.post("/v1/current-address", EmployeeController.insertEmployeeCurrentAddress);
+router.post("/v1/permanent-address", EmployeeController.insertEmployeePermanentAddress);
+router.post("/v1/bulk-data-entry", EmployeeController.insertBulkEmployees);
+router.get("/v1/employee", EmployeeController.viewAllEmployeesEverything)
 
 // test apis here 🤡
 router.get("/test", async (req, res) => {
