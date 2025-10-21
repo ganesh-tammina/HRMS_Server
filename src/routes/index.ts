c0ac8c411a5bf573fc3e54c5549a289e984bf40a
ganesh-tammina <82147644+ganesh-tammina@users.noreply.github.com>
2025-10-21 19:47:41 +0530

    made changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,20 +14,20 @@ const upload = multer({ dest: "uploads/" });
 
 const router = Router();
 router.post("/v1/send-email", MailController.mailsender);
-router.post("/v1/check-email",checkWhoAmI, EmployeeLoginController.EmailCheck);
+router.post("/v1/check-email", checkWhoAmI, EmployeeLoginController.EmailCheck);
 router.post("/v1/gen-password", checkIfIamValidEmployee, EmployeeLoginController.PasswordGeneratorHey)
 router.post("/v1/login", checkIfIamEmployeeAtAll, EmployeeLoginController.Login)
 router.post("/v1/addEmployee", EmployeeController.insertEmployee);
 router.post("/v1/addEmployementDetails", EmployeeController.insertEmploymentDetails);
 router.post("/v1/add-Statutory-Info", EmployeeController.insertEmployeeStatutoryInfo);
 router.post("/v1/add-Employee-Family-Info", EmployeeController.insertEmployeeFamilyInfo);
-router.post("/v1/parse-excel", upload.single("file"),AdminController.uploadExcel);
+router.post("/v1/parse-excel", upload.single("file"), AdminController.uploadExcel);
 router.post("/v1/addExitDetails", EmployeeController.insertExitDetails);
 router.post("/v1/current-address", EmployeeController.insertEmployeeCurrentAddress);
 router.post("/v1/permanent-address", EmployeeController.insertEmployeePermanentAddress);
 router.post("/v1/bulk-data-entry", EmployeeController.insertBulkEmployees);
-router.post("/v1/employee",verifyAccessToken, checkMyRole, EmployeeController.viewAllEmployeesEverything);
-router.get("/v1/log-out", EmployeeLoginController.LogOut)
+router.post("/v1/employee", verifyAccessToken, checkMyRole, EmployeeController.viewAllEmployeesEverything);
+router.post("/v1/log-out", EmployeeLoginController.LogOut)
 
 // test apis here 🤡
 router.get
